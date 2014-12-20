0720ba9c48119d47d8cc343f8e01f37e0dae01c6
Yuriy Aleksandrov <george2code@gmail.com>
2014-12-20 14:03:52 +0200

    twitter bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$( document ).ready(function() {
+    $('.status').hover(function() {
+        $(this).toggleClass('hover')
+    });
+});
+
