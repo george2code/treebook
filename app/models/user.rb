e5aa8cdf9aaf58b10bdaedc25cfe91e9b828cac3
Yuri Aleksandrov <george2code@gmail.com>
2014-12-23 15:52:58 +0200

    Generated profile controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me,
-                  :first_name, :last_name, :profile_name
-
   validates :first_name, presence: true
 
   validates :last_name, presence: true
@@ -15,7 +11,7 @@ class User < ActiveRecord::Base
   validates :profile_name, presence: true,
             uniqueness: true,
             format: {
-                with: /^[a-zA-Z0-9_-]+$/,
+                with: /[a-zA-Z0-9_-]+/,
                 message: 'Must be formatted correctly.'
             }
 
